03ba72b2726012309ed868f8913ec5ad0c020ea8
Hasibur Rahman <hasibonline2@gmail.com>
2019-02-02 14:08:37 +0600

    Updates

---
+++ precache-manifest.c5ad38fdf73e4d3226ca1cc3985d1268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceb00c122ffb25386617",
+    "url": "/Random-Quote-Machine/static/css/main.5c1053c3.chunk.css"
+  },
+  {
+    "revision": "ceb00c122ffb25386617",
+    "url": "/Random-Quote-Machine/static/js/main.ceb00c12.chunk.js"
+  },
+  {
+    "revision": "d3cedc948769800c1220",
+    "url": "/Random-Quote-Machine/static/css/1.b6f381db.chunk.css"
+  },
+  {
+    "revision": "d3cedc948769800c1220",
+    "url": "/Random-Quote-Machine/static/js/1.d3cedc94.chunk.js"
+  },
+  {
+    "revision": "67122e2cdde974b7bf57",
+    "url": "/Random-Quote-Machine/static/js/runtime~main.67122e2c.js"
+  },
+  {
+    "revision": "b1432d2233253015e8de8580d4a29242",
+    "url": "/Random-Quote-Machine/index.html"
+  }
+];
